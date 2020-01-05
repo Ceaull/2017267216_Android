618e8e22cae51137747e61263ad1ba88fbda53f3
Ceaull <948220613@qq.com>
2020-01-05 19:48:07 +0800

    关于服务的程序-下载文件

---
+++ project8/app/src/main/java/cn/edu/hstc/pro8/DownloadListener.java
@@ -0,0 +1,9 @@
+package cn.edu.hstc.pro8;
+
+public interface DownloadListener {
+    void onProgress(int progress);
+    void onSuccess();
+    void onFailed();
+    void onPaused();
+    void onCanceled();
+}
